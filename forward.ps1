7f7d15af6a7f498d6ffe0308e22ce28c42f7f894
Julian Legler <julian.legler@campus.tu-berlin.de>
2024-07-25 13:07:05 +0200

    feat(dev): add script to forward kubernetes ports feat(dev): add config files to properly run the module in vs code fix(dev): fix problem with incompatible versions

---
+++ forward.ps1
@@ -0,0 +1,5 @@
+kubectl port-forward --address localhost -n kepler service/kepler-exporter 9102:9102 &
+kubectl port-forward --address localhost -n monitoring service/prometheus-k8s 9090:9090 &
+kubectl port-forward --address localhost -n monitoring service/grafana 3000:3000 &
+
+kubectl port-forward --address localhost -n default service/jupyter 80:80 &
