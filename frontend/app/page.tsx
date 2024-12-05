db1580d2ba19e86f4ab8bb09073e3175fef47600
saramakishti <saramakishti@gmail.com>
2024-12-05 14:03:27 +0100

    removed unused imports

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Telescope } from "lucide-react";
 import StartExperimentDialog from "@/components/dashboard/start-experiment";
-// import Metrics from "@/components/dashboard/metrics";
 
 export default function Home() {
   return (
@@ -16,7 +15,6 @@ export default function Home() {
         </Button>
       </div>
       <div className="my-4">
-        {/* <Metrics /> */}
         Chart coming soon...
       </div>
     </div>
