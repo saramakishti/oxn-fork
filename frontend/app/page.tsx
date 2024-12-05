53b8e7f5abb1159e7b5285164e7b3b883d926eb1
saramakishti <saramakishti@gmail.com>
2024-12-05 13:25:13 +0100

    updated dependencies

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Telescope } from "lucide-react";
 import StartExperimentDialog from "@/components/dashboard/start-experiment";
-import Metrics from "@/components/dashboard/metrics";
+// import Metrics from "@/components/dashboard/metrics";
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
         </Button>
       </div>
       <div className="my-4">
-        <Metrics />
+        {/* <Metrics /> */}
         Chart coming soon...
       </div>
     </div>
